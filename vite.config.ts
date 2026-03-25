58a7f532a77f7d8ce7f367092ec28f30b0423230
2000krysztof <2000krzysztof@gmail.com>
2026-03-25 20:40:24 +0000

    change base in vit config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "2000krysztof/final-year-project-poster.git"
+  base: "/final-year-project-poster/"
 })
